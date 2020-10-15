0910969963884ec881cf542ac599ca95cbfa7aad
Tymoteusz Stokarski <tjoelstokarski@gmail.com>
2020-10-15 17:59:48 +0200

    Initial Commit

---
+++ NumbersApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NumbersApplication
+//
+//  Created by Tymoteusz Stokarski on 15/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
